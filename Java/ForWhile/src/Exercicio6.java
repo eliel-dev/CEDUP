d177a42c7b8d9b3b980868c5d5562bbe136b9c0f
Eliel <126355234+eliel-dev@users.noreply.github.com>
2023-03-10 14:01:54 -0300

    ForWhile

--- Java/ForWhile/src/Exercicio6.java
+++ Java/ForWhile/src/Exercicio6.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
+/**Escreva um programa que pergunte ao usuário, por um laço while,
+várias notas maiores ou iguais à zero.
+Por fim, quando o usuário digitar uma nota negativa, o programa deve mostrar
+a média aritmética.*/
+
 public class Exercicio6 {
     public static void main(String[] args) {
+        Scanner leTeclado = new Scanner(System.in);
+        int i = 0;
+        double nota, soma = 0D;
 
+        System.out.println("Informe as notas (Informe uma nota negativa para encerrar)");
 
+        while(true){
+            nota = leTeclado.nextDouble();
+            if (nota < 0D){
+                break;
+            }
+            soma += nota;
+            i++;
+        }
+        if (i==0){
+            System.out.println("Não foi informado uma nota");
+        }else {
+            double media = soma/i;
+            System.out.println("Media das notas:"+media);
+        }
+        leTeclado.close();
     }
 }
--- Java/ForWhile/src/Exercicio7.java
+++ Java/ForWhile/src/Exercicio7.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
+/**7. Escreva um programa imprima o seguinte menu com 3 funcionalidades de
+        calculadora:
+        ''''''''''''''''''''''
+        Calculadora CEDUP
+        1 - Somar
+        2 - Subtrair
+        3 - Sair
+        ''''''''''''''''''''''
+        O programa deve exibir o menu novamente para qualquer opção escolhida,
+        exceto para a opção 3, que finaliza a execução.*/
 public class Exercicio7 {
     public static void main(String[] args) {
+        Scanner leTeclado = new Scanner(System.in);
+
+        System.out.println(
+            "Calculadora CEDUP\n" +
+            "1 - Somar\n" +
+            "2 - Subtrair\n" +
+            "3 - Sair"
+        );
+
+        int opcao = leTeclado.nextInt();
+        while (opcao != 3){
+            System.out.println("ola mundo");
+            break;
+        }
+
+
+
 
     }
 }
