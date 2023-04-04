3046827b55e27bcaf5dd3bdc48045dae76581c78
Eliel <126355234+eliel-dev@users.noreply.github.com>
2023-04-04 07:15:07 -0300

    Matriz

--- TÉCNICAS DE PROGRAMAÇÃO/Java/Vetores/src/Exercicio4.java
+++ TÉCNICAS DE PROGRAMAÇÃO/Java/Vetores/src/Exercicio4.java
@@ -1,28 +1,39 @@
+/** 4. Faça um programa que leia o nome e a idade de 5 pessoas e salve-os em uma matriz.
+ * Por fim mostre a média das idades. */
+
 import java.util.Scanner;
 
 public class Exercicio4 {
 
     public static void main(String[] args) {
-
+        // Le as entradas digitadas pelo user
         Scanner entrada = new Scanner(System.in);
 
+        //Matriz de 5 linhas e 2 colunas
         String[][] pessoas = new String[5][2];
 
-
+        //Loop para preencher os nomes e idades informadas pelo user
         for (int i = 0; i < pessoas.length; i++){
             System.out.println("Informe seu nome: ");
             pessoas [i][0] = entrada.nextLine();
             System.out.println("inform a Idade: ");
             pessoas [i][1] = entrada.nextLine();
-        }
-
-
-        for (int i = 0; i < pessoas.length; i++) {
-            for (int j = 0; j < pessoas[0].length; j++) {
-                System.out.print(pessoas[i][j] + " \t");
-            }
             System.out.println();
         }
+
+        //Variaveis para armazenar a soma das idades e a média
+        int somaIdades = 0;
+        double mediaIdades;
+
+        for (int i = 0; i < pessoas.length; i++) {
+            int idade_Int = Integer.parseInt(pessoas[i][1]);
+            somaIdades += idade_Int;
+        }
+
+        mediaIdades = (double) somaIdades / pessoas.length;
+
+        System.out.println("A média das idades é: " + mediaIdades);
+
     }
 }
 
